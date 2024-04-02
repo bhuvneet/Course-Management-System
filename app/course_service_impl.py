ac09fdb13940bb4f95891ebf70c09660b09124ef
Bhuvneet Thakur <78770635+bhuvneet@users.noreply.github.com>
2024-04-01 21:49:20 -0400

    Added additional comments and created more methods for unit testing

--- app/course_service_impl.py
+++ app/course_service_impl.py
@@ -11,14 +11,18 @@ class CourseServiceImpl(CourseService):
     
     # this method returns the list of courses
     def get_courses(self) -> List[str]:
+        # return a list of all courses
         return list(self.courses.values())
     
     # this method is called to get course by it's ID
     def get_course_by_id(self, course_id: int) -> str:
+        # return name of course by ID
         return self.courses.get(course_id, "Course not found")
     
     # this method is called to create a course and return it's ID
     def create_course(self, course_name: str) -> int:
+        # create a new course by ID
+        # return ID of the newly created course
         course_id = len(self.courses) + 1
         self.courses[course_id] = course_name
         return course_id
@@ -28,9 +32,16 @@ class CourseServiceImpl(CourseService):
         # find course and remove it from dictionary
         if course_id in self.courses:
             del self.courses[course_id]
+            if course_id in self.assignments:
+                del self.assignments[course_id]
+            if course_id in self.enrollments:
+                del self.enrollments[course_id]
+            if course_id in self.grades:
+                del self.grades[course_id]
             return True
         return False
     
+    # this method is called to create assignment for a course
     def create_assignment(self, course_id, assignment_name) -> int:
             # check if assignment is in the courses dictionary
             # if not, add it to the dictionary
@@ -40,7 +51,10 @@ class CourseServiceImpl(CourseService):
             self.assignments[course_id][assignment_id] = assignment_name
             return assignment_id
     
+    # this method enrolls a student in a course
     def enroll_student(self, course_id, student_id) -> bool:
+        # enroll student in a course, 
+        # return True if student was enrolled successfully, otherwise False
         if course_id not in self.enrollments:
             self.enrollments[course_id] = set()
         self.enrollments[course_id].add(student_id)
@@ -48,35 +62,46 @@ class CourseServiceImpl(CourseService):
     
     # this method is called when a student drops out of a course
     def dropout_student(self, course_id, student_id) -> bool:
+        # find student and course ID in enrollments, and remove
         if course_id in self.enrollments and student_id in self.enrollments[course_id]:
             self.enrollments[course_id].remove(student_id)
             return True
         return False    
     
-    def submit_assignment(self, course_id, student_id, assignment_id, grade: int) -> bool:
+    # this method is called to submit an assignment for a student for a course
+    def submit_assignment(self, course_id, student_id: int, assignment_id, grade: int) -> bool:
+        # submit assignment for the student for particular course ID
         if course_id in self.enrollments and student_id in self.enrollments[course_id]:
             if course_id not in self.grades:
                 self.grades[course_id] = {}
             if assignment_id not in self.grades[course_id]:
                 self.grades[course_id][assignment_id] = {}
-            self.grades[course_id][assignment_id][student_id] = grade
+            if 0 <= grade <= 100:
+                # grades cannot be less than 0 or greater than 100     
+                self.grades[course_id][assignment_id][student_id] = grade
             return True
         return False
     
+    # this method is called get the avg of an assignment
     def get_assignment_grade_avg(self, course_id, assignment_id) -> int:
+        # calculate avg of grades of an assignment by course ID
         if course_id in self.grades and assignment_id in self.grades:
             student_grades = self.grades[course_id][assignment_id].values()
             return sum(student_grades) // len(student_grades) if student_grades else 0
         return 0
     
+    # this method is called to get grade avg of students
     def get_student_grade_avg(self, course_id, student_id) -> int:
+        # calculate students avg of a course
         if course_id in self.grades:
             student_grades = [self.grades[course_id][assignment_id].get(student_id, 0)
                               for assignment_id in self.grades[course_id]]
             return sum(student_grades) // len(student_grades) if student_grades else 0
         return 0
     
+    # this method is called to get the top 5 students in a course
     def get_top_five_students(self, course_id) -> List[int]:
+        # get top five students by course ID
         if course_id in self.grades:
             avg_grades = {}
             for assignment_id in self.grades[course_id]:
@@ -88,34 +113,6 @@ class CourseServiceImpl(CourseService):
             return [student[0] for student in sorted_students[:5]]
         return []
 
-# for testing
-course_service = CourseServiceImpl()
-
-course_id = course_service.create_course("Python Programming")
-print("Course ID: ", course_id)
-
-# enroll a student
-student_id = 1
-course_service.enroll_student(course_id, student_id)
-
-# create asssignment
-assignment_id = course_service.create_assignment(course_id, "Assignment 1")
-
-# submit assignment
-grade = 85
-course_service.submit_assignment(course_id, student_id, assignment_id, grade)
-
-# get assignment grade avg
-assignment_avg_grade = course_service.get_assignment_grade_avg(course_id, assignment_id)
-print("Average grade for assignment 1: ", assignment_avg_grade)
-
-# get courses list
-course_list = course_service.get_courses()
-print("Course list: ", course_list)
-
-# get top 5 students
-top_five_students = course_service.get_top_five_students(course_id)
-print("Top five students in th course: ", top_five_students)
-    
+   
     
     
--- app/unit_tests.py
+++ app/unit_tests.py
@@ -3,6 +3,14 @@ import unittest
 # Import CourseServiceImpl class
 from course_service_impl import CourseServiceImpl
 
+# this class tests and verifies the following methods of CourseServiceImpl class:
+# - create_course
+# - get_course_by_id
+# - course_list
+# - enroll_students
+# - create_assignment
+# - submit_assignment
+
 class UnitTestCourseServiceImp(unittest.TestCase):
     def setUp(self):
         self.course_service = CourseServiceImpl()
@@ -25,8 +33,27 @@ class UnitTestCourseServiceImp(unittest.TestCase):
     def test_course_list(self):
         course_list = self.course_service.get_courses()
         
-    def enroll_student(self):
-        enroll_std = self.course_service.enroll_student(1, 1)
+    def test_enroll_student(self):
+        # enroll students
+        student_id = 1
+        self.course_service.enroll_student(1, student_id)
+        student_id = 2
+        self.course_service.enroll_student(1, student_id)
+        student_id = 3
+        self.course_service.enroll_student(1, student_id)
+        student_id = 4
+        self.course_service.enroll_student(1, student_id)
+        
+    def test_create_assignment(self):
+        self.course_service.create_assignment(1, "Assignment 1")
+        
+    def test_submit_assignment(self):
+        course_id=1
+        assignment_id="Assignment 1"
+        self.course_service.submit_assignment(course_id, student_id=1, assignment_id=assignment_id, grade=85)
+        self.course_service.submit_assignment(course_id, student_id=2, assignment_id=assignment_id, grade=64)
+        self.course_service.submit_assignment(course_id, student_id=3, assignment_id=assignment_id, grade=71)
+        self.course_service.submit_assignment(course_id, student_id=4, assignment_id=assignment_id, grade=78)
         
 
 if __name__ == '__main__':
